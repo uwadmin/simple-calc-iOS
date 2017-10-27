9e752526963b97114ee189559789c727e1cd3aa3
Chao Ma <ynyounuo@me.com>
2017-10-26 18:26:13 -0700

    Add second view controller

---
+++ simple-calc-ios/HistViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HistViewController.swift
+//  simple-calc-ios
+//
+//  Created by ​ on 10/26/17.
+//  Copyright © 2017 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class HistViewController: UIViewController {
+    @IBOutlet weak var ScrollView: UIScrollView!
+    
+    let hist = UILabel()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
