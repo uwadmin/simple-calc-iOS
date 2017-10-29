269ab8a5f25b4d50d937c98e1528a69b65461dc8
Chao Ma <ynyounuo@me.com>
2017-10-29 14:42:14 -0700

    Add a title to the main screen

--- simple-calc-ios/HistViewController.swift
+++ simple-calc-ios/HistViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class UILabelPadding: UILabel {
     let padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+    
     override func drawText(in rect: CGRect) {
         super.drawText(in: UIEdgeInsetsInsetRect(rect, padding))
     }
+    
     override var intrinsicContentSize : CGSize {
         let superContentSize = super.intrinsicContentSize
         let width = superContentSize.width + padding.left + padding.right
